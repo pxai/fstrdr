2a96c19c7bbd53b24ed85ec840af521e46476e07
pxai <pello.altadill@gluckgames.com>
2020-11-26 16:42:42 +0100

    Added random class

---
+++ src/random.js
@@ -0,0 +1,18 @@
+const random = max => randomWithin(0, max);
+const randomWithin = (min, max) => Math.round(Math.random() * (max - min)) + min;
+const randomIndex = list => randomWithin(0, list.length - 1);
+const shuffle = list => {
+    let a, b, tmp;
+    let shuffled = list.slice();
+    shuffled.forEach( element => {
+        a = randomIndex(shuffled);
+        b = randomIndex(shuffled);
+        tmp = shuffled[a];
+        shuffled[a] = shuffled[b];
+        shuffled[b] = tmp;
+    });
+
+    return shuffled;
+};
+
+module.exports = { random, randomWithin, randomIndex, shuffle };
---
+++ test/unit/random.spec.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { random, randomWithin, randomIndex, shuffle } = require('../../src/random');
+
+describe('basic testing for random utils', () => {
+  it('should return random number', () => {
+      expect(random(5)).to.be.within(0, 5);
+  });
+
+  it('should return random number within min and max', () => {
+      expect(randomWithin(3, 10)).to.be.within(3, 10);
+  });
+
+  it('should return a randomIndex from array', () => {
+      const elements = [2, 5, 7, 2, 7, 1, 3];
+      expect(randomIndex(elements)).to.be.within(0, elements.length - 1);
+  });
+
+  it('should shuffle values', () => {
+      const elements = [2, 5, 7, 2, 7, 1, 3];
+      const shuffled = shuffle(elements);
+
+      expect(shuffled.length).to.equal(elements.length);
+      expect(shuffled).to.not.deep.equal(elements);
+  });
+});
