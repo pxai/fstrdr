6ea7d25ffc7937b38144d01773bbd2cddb49925b
pxai <pello.altadill@gluckgames.com>
2021-01-04 19:18:31 +0100

    Passing with skips looking like shit

---
+++ test/unit/components/memory/index.spec.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from 'enzyme';
+import { Context, ContextProps } from '../../../../src/context';
+import Memory from '../../../../src/components/memory';
+
+describe('Memory component', () => {
+  let context: ContextProps;
+
+  beforeEach(() => {
+      context = {
+          config: {
+              name: "sample",
+              version: 666
+          },
+          auth: {
+            loggedIn: true
+          }
+      }
+  });
+
+  it('renders correctly', () => {
+    const memory = renderer.create(<Context.Provider value={context}><Memory /></Context.Provider>).toJSON();
+    expect(memory).toMatchSnapshot();
+  });
+
+  it('shows footer links correctly', () => {
+    const memory = render(<Context.Provider value={context}><Memory /></Context.Provider>);
+  });
+
+  it('shows text correctly', () => {
+    const memory = render(<Context.Provider value={context}><Memory /></Context.Provider>);
+
+    expect(memory.find('div').text()).toEqual("THIS IS MEMORY");
+  });
+});
